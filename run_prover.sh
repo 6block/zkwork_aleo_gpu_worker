2134f98b8fc3f7091983ecbbfc1145c38c1673df
lovedret <lovedret@6block.com>
2024-09-02 10:45:30 +0800

    Update run_prover script

--- run_prover.sh
+++ run_prover.sh
@@ -6,10 +6,10 @@ ssl_server=aleo.hk.zk.work:10003
 reward_address=aleo1xxx...
 
 # set your own custom name
-custom_name=""
+custom_name="zkwork_worker"
 
 # try change --gpu_index to select the gpu you want to solve puzzle
 # tcp connected
-./aleo_prover --address $reward_address --tcp_server $tcp_server --custom_name $custom_name 2>&1
+nohup ./aleo_prover --address $reward_address --tcp_server $tcp_server --custom_name $custom_name >> prover.log 2>&1 &
 # ssl connected
-./aleo_prover --address $reward_address --ssl  --ssl_server $ssl_server --custom_name $custom_name 2>&1
+# nohup ./aleo_prover --address $reward_address --ssl  --ssl_server $ssl_server --custom_name $custom_name >> prover.log 2>&1 &
